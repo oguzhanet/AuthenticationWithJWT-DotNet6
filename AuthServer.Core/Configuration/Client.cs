dec0560876a5dd76fec935c42b1b0428d748d6ff
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-06-04 19:21:11 +0300

    4.6.2022

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTOs;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
