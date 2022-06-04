c53cbece67e74b4d8bf8c814c8f7fbcf7a035821
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-06-04 14:03:17 +0300

    4.6.2022

---
+++ AuthServer.Core/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthServer.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        TEntity GetAllAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task<TEntity> GetByIdAsync(int id);
+        
+        Task AddAsync(TEntity entity);
+        
+        TEntity UpdateAsync(TEntity entity);
+        
+        void Remove(TEntity entity);
+    }
+}
