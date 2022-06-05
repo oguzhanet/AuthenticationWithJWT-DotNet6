b391ba40ac84112cbee262762b94ea88ea490779
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-06-05 16:01:28 +0300

    5.6.2022

---
+++ AuthServer.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AuthServer.DataAccess.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+            _dbSet = appDbContext.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+
+            return entity;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public TEntity UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+
+            return entity;
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.Where(filter);
+        }
+    }
+}
---
+++ AuthServer.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
