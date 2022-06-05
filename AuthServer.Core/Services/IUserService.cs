fa4535ae283f09084a15fe15eaf8ed350d7d06b2
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-06-05 13:57:42 +0300

    5.6.2022

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTOs;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
---
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthServer.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
